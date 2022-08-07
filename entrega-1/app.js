c96b83a05d6b0cf3f1e7ff4d61a221af0bac4fb6
juanbur <78306886+juanbur@users.noreply.github.com>
2022-08-07 09:45:48 +0200

    Add files via upload

---
+++ entrega-1/app.js
@@ -0,0 +1,38 @@
+//entrega:
+    //incorporar variables, bucles, todo adentro de una función
+    //bucle funcion y switch
+    //if o switch adentro de una funcion
+
+    // console.log(" hola");
+    //funcion para solicitar nombre
+// function solicitarNombre (){
+//     for(i=0;i<3;i++){
+//         let nombre = prompt("ingresa tu nombre")
+//         alert ("el nombre ingresado es " + nombre)
+//     }
+// }
+// solicitarNombre()
+
+// pensamos qué necesitamos para realizar un calculador de precio final
+//funcion que le sume el 0.21%
+//funcion que sume el precio de origen + IVA
+//funcion que le calcule el descuento
+
+// const iva = prod => prod * 0.21
+
+// const precioMasIva = (producto, ivaProd) => producto + ivaProd;
+
+// const descuento = (precioIvaFinal, descuentoFinal) => precioIvaFinal - descuentoFinal;
+
+// let productoUsuario = Number (prompt("ingresa el costo de tu producto"));
+// // console.log(precioIva(productoUsuario, iva(productoUsuario)))
+// let descuentoProducto = 100;
+
+// //calcular el precioProd + IVA - descuento
+// let prodConIvaMenosDesc = descuento(precioMasIva(productoUsuario,iva(productoUsuario)),descuentoProducto)
+// // let nuevoPrecioFinal = descuento(precioIva(productoUsuario, precioIva(productoUsuario)),descuento)
+// console.log(prodConIvaMenosDesc)
+
+
+// console.log(nuevoPrecioFinal)
+
